3f2f7414b98f6f2def7790fbb486aacfbfa27b9d
Oskenso Kashi <contact@oskenso.com>
2013-09-03 23:20:24 -0700

    General Improvements

--- app.js
+++ app.js
@@ -22,7 +22,6 @@
 
 // Settings here should be modified
 var host = 'http://e621.net/';
-var username = "Oskenso";
 var download_folder = 'images/';
 var chunk_count = 20; // amount of chunks to download
 var chunk_size = 20; // image files per chunk
@@ -58,7 +57,12 @@ function getJson(opt, chunk_id, callback) {
 		});
 		
 		res.on('end', function() {
-			callback(JSON.parse(data), chunk_id);
+//			console.log(JSON.parse(data).length);
+			var parsed = JSON.parse(data);
+			if (parsed.length == 0)
+				callback(null, chunk_id);
+			else
+				callback(JSON.parse(data), chunk_id);
 		});
 	});
 }
@@ -77,27 +81,42 @@ function downloadFile(opt, file_path, callback) {
 
 // added recent http hack for e621's new json scheme :o
 // TODO: impliment require('https') for https implementation
+
+var tags = '';
+var args = process.argv.slice(2);
+
+args.forEach(function(val, index, array) {
+	tags += val + ' ';
+});
+tags = tags.trim();
+
+var totalImages = 0;
+
 for (var x = 0; x < chunk_count; x++) {
 	console.log('Chunk ' + x + ': Downloading image list.');
 	var	opts = req_opt;
-	opts.path = '/post/index.json?tags=fav:'+username+' order:score_desc -flash&page=45&limit=' + chunk_size + '&page='+x;
+	opts.path = '/post/index.json?tags=' + tags + '&page=45&limit=' + chunk_size + '&page=' + (x + 1);
 	getJson(opts, x, function(a, cid) {
 	
-		console.log('<' + 'Chunk ' + (cid + 1) + ': Image list downloaded.' + '>');
-		//console.log('Chunk ' + cid + ': Images:');
-		//for (l = 0; l < a.length; l++) {console.log('\t' + 'http' + a[l].file_url.substr(5));}
-		//console.log('\n');
+		if (a) {
+			totalImages += a.length;
+	
+			console.log('<' + 'Chunk ' + (cid + 1) + ': Image list downloaded.' + '>');
+			//console.log('Chunk ' + cid + ': Images:');
+			//for (l = 0; l < a.length; l++) {console.log('\t' + 'http' + a[l].file_url.substr(5));}
+			//console.log('\n');
 		
-		for (i = 0; i < a.length; i++) {
+			for (i = 0; i < a.length; i++) {
 			
-			var filename = a[i].file_url.substr(a[i].file_url.lastIndexOf("/") + 1);
-			var opts = req_opt;
-			opts.path = a[i].file_url;
+				var filename = a[i].file_url.substr(a[i].file_url.lastIndexOf("/") + 1);
+				var opts = req_opt;
+				opts.path = a[i].file_url;
 			
-			downloadFile(opts, filename, function(fp) {
-				files_downloaded++;
-				console.log('"' + fp + '" saved. ' + files_downloaded + '/' + (chunk_count * chunk_size) +' downloaded.');
-			});
+				downloadFile(opts, filename, function(fp) {
+					files_downloaded++;
+					console.log('"' + fp + '" saved. ' + files_downloaded + '/' + totalImages +' downloaded.');
+				});
+			}
 		}
 	});
 }
